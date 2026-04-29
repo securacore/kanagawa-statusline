e931c905d8103e1b6794ef609f6b2b55bc6e513a
Jon Tech <jon@securacore.net>
2026-04-29 12:48:51 -0500

    Release v0.0.14

--- statusline.sh
+++ statusline.sh
@@ -67,7 +67,7 @@
 # remote check fetches /VERSION (cheap, ~10 bytes) and compares to this
 # constant. `kanagawa-statusline update` rewrites the whole script so this
 # value moves with each release.
-KANAGAWA_STATUSLINE_VERSION="0.0.13"
+KANAGAWA_STATUSLINE_VERSION="0.0.14"
 
 input=$(cat)
 
